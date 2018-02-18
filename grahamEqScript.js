6c3a99fd35f798b4a377f8dc22ffea88c19954e7
Stevaavo <steveGithub@trambert.com>
2018-02-18 14:17:50 -0500

    Value-change animations are all good!

--- grahamEqScript.js
+++ grahamEqScript.js
@@ -28,11 +28,21 @@ var resetToDefaultLink = document.querySelector('#resetToDefaultLink');
 
 // This function causes an element to "pulse," and is used when the javascript fills in valus (after calculating or resetting parameters to defaults)
 function pulseElement(elementToBePulsed) {
-	elementToBePulsed.classList.add('pulseOn');
+
+	elementToBePulsed.classList.add('pulseOn'); // This adds a green background to the box
+
+	// After waiting a beat (100ms chosen arbitrarily) we add a class that causes all changes to the element to be animated, and then we remove the 'pulseOn' class, so that the border background to fade away.
 	setTimeout(
 		function() {
+			elementToBePulsed.classList.add('changesAreAnimated');
 			elementToBePulsed.classList.remove('pulseOn');
 		}, 100);
+
+	// Once that animation is complete (I assume by 1 second later), we remove the changesAreAnimated class, so that when we set the background back to green on the next click it happens suddenly without being animated in.  I think that looks cooler.
+	setTimeout(
+		function() {
+			elementToBePulsed.classList.remove('changesAreAnimated');
+		}, 500);
 }
 
 
@@ -41,14 +51,16 @@ function pulseElement(elementToBePulsed) {
 // This function fills in the "Advanced Parameters" input boxes with default values.  It is called at pageload and when the "reset to defaults" link is clicked.
 function setAdvancedParametersToDefaults() {
 	salaryMultiplierBox.value = '1.5';
-	pulseElement(salaryMultiplierBoxGroup);
-
-	// companyProfitBox.value = '50';
-	// pulseElement(companyProfitBox);
+	companyProfitBox.value = '50';
 }
 
 // Sets "Advanced Parameters" to default on pageload
 setAdvancedParametersToDefaults();
 
-resetToDefaultLink.addEventListener("click", setAdvancedParametersToDefaults);
+resetToDefaultLink.addEventListener("click", function() {
+	setAdvancedParametersToDefaults
+	pulseElement(salaryMultiplierBox);
+	pulseElement(companyProfitBox);
+
+});
 
